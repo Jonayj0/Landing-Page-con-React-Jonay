a2d5dea724a5438a4440faf488ec18ae094fdf1d
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-01 14:18:56 +0000

    Landing page Jonay con props React

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Footer = () => {
 	return (
-    // <div className="card text-center">
-		<div className="footer-expand-lg bg-dark text-center text-white pb-5">
-    <p>Copyrigth Your Website 2019</p>
+    <div className="card text-center">
+		<div className="footer-expand-lg bg-dark text-center text-white py-5">
+    <p>Copyrigth © Jonay Website 2024</p>
         </div>
-    // </div>
+    </div>
 	);
 };
 
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Jumbotron = () => {
 	return (
-		<div className="jumbotron bg-secondary mx-2 mb-4 ps-4 pb-5 rounded-bottom-2">
+		<div className="jumbotron bg-secondary mx-2 my-4 ps-4 pb-5 rounded">
   <h1 className="display-4">A Warm Welcome!</h1>
   {/* <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr className="my-4"/> */}
   <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta)</p>
   <p className="lead">
-    <a className="btn btn-primary btn-md" href="#" role="button">Call to action!</a>
+    <a className="btn btn-primary btn-md" href="https://www.elmundoenbandeja.com/" role="button">Call to action!</a>
   </p>
 </div>
 	);
